c068111c85292b5c87e5a4ff046d2ee7cf96a413
LittleWhite <xiaolinggnb@gmail.com>
2021-09-16 15:21:08 +0800

    This is the same file with 第一题.cpp,just changed its name for typing conviniently.And it's been refined to fix the bug come from using changed-in-time que_check.size() as loop limit.

---
+++ local_first.cpp
@@ -0,0 +1,38 @@
+#include<fstream>
+#include<queue>
+#include<iostream>
+using namespace std;
+int main() {
+	ifstream infile("ÓÃÀýÊäÈë (1).txt", ios::in);
+	int number;
+	infile >> number;
+	queue<int>que_original;
+	for (int i = 0; i < number; i++) {
+		int temp;
+		infile >> temp;
+		que_original.push(temp);
+	}
+	queue<int>que;
+	queue<int>que_check; 	//check if front in original repeat with que.
+	int answer = 1;
+	for (int i = 0; i < number; i++) {
+		int front;
+		front = que_original.front(); que_original.pop();
+		bool repeat_flag = false;
+		int size = que_check.size();
+		for (int j = 0; j < size; j++) {
+			int temp;
+			temp = que_check.front(); que_check.pop(); 
+			if (temp == front) {
+				repeat_flag = true;
+				break;
+			}
+		}
+		if (repeat_flag) que = que_check;
+		que.push(front);
+
+		que_check = que;	//assign here,prepare for next loop
+		if (answer < que.size()) answer = que.size();
+	}
+	cout << answer;
+}
